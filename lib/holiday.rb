466e7ef45280d34cb1a997e436dad53d9cd6edc4
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-02 13:35:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -124,8 +124,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, array|
-      if array.include?("BBQ")
-        puts holiday 
+      puts holiday  if array.include?("BBQ")
       
       
     end
