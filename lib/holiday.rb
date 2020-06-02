8ce0a04f5a746e4c7e66554895d6ec8f37c3f96f
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-02 13:18:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,7 +103,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, event|
     puts season.to_s.capitalize + ":"
     event.each do |event, items|
-      puts "  " + event.to_s.capitalize + ": " + items.join(", ")
+      event.to_s.split!("_").join(" ")
+      puts "  " + event.capitalize + ": " + items.join(", ")
     
     end
   end
