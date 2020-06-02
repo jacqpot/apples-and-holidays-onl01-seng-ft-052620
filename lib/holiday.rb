aec8e34356ded8af228aed103cba19b6246116cd
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-02 13:01:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].map {|holiday, suply| suply }
+    
 end
 
 def all_supplies_in_holidays(holiday_hash)
